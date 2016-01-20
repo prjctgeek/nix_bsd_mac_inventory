f2fbb7430069ac5ab47002c9f140d71bd8c3a3f4
Brook, Andy [BSD] - CRI <abrook@bsd.uchicago.edu>
2016-01-20 10:55:20 -0600

    Update module_freebsd.py to use fabric instead of paramiko

--- module_freebsd.py
+++ module_freebsd.py
@@ -1,85 +1,55 @@
 import sys
 import math
-import paramiko
 import util_uploader
+# Import Fabric API
+from fabric.api import *
 
 
 
 class GetBSDData():
-    def __init__(self,  ip, SSH_PORT, TIMEOUT, usr, pwd, USE_KEY_FILE, KEY_FILE, \
-                    GET_SERIAL_INFO, GET_HARDWARE_INFO, GET_OS_DETAILS, \
+    def __init__(self, GET_SERIAL_INFO, GET_HARDWARE_INFO, GET_OS_DETAILS, \
                     GET_CPU_INFO, GET_MEMORY_INFO, IGNORE_DOMAIN, UPLOAD_IPV6, DEBUG):
-                        
-        self.machine_name      = ip
-        self.port              = int(SSH_PORT)
-        self.timeout           = TIMEOUT
-        self.username          = usr
-        self.password          = pwd
-        self.ssh               = paramiko.SSHClient()
-        self.USE_KEY_FILE      = USE_KEY_FILE
-        self.KEY_FILE          = KEY_FILE
+
         self.GET_SERIAL_INFO   = GET_SERIAL_INFO
         self.GET_HARDWARE_INFO = GET_HARDWARE_INFO
         self.GET_OS_DETAILS    = GET_OS_DETAILS
         self.GET_CPU_INFO      = GET_CPU_INFO
-        self.GET_MEMORY_INFO   = GET_MEMORY_INFO 
-        self.IGNORE_DOMAIN     = IGNORE_DOMAIN       
+        self.GET_MEMORY_INFO   = GET_MEMORY_INFO
+        self.IGNORE_DOMAIN     = IGNORE_DOMAIN
         self.UPLOAD_IPV6       = UPLOAD_IPV6
         self.DEBUG             = DEBUG
-        self.ssh               = paramiko.SSHClient()
-        self.conn              = None
         self.sysData           = {}
         self.allData           = []
-        
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
 
     def main(self):
-        self.connect()
         self.get_sys()
         self.get_CPU()
         self.get_RAM()
-        self.allData.append(self.sysData)    
         self.get_IP()
+        self.allData.append(self.sysData)
         return self.allData
 
+    def execute(cmd):
+        # Since there seems to be no sudo commands for this module
+        output = run(cmd)
+        data_err = output.stderr
+        data_out = output.stdout
+        return data_out.splitlines(),data_err.splitlines()
 
-    def connect(self):
-        #self.conn = self.ssh.connect(ip, username=usr, password=pwd, timeout=TIMEOUT)
-        try:
-            if not self.USE_KEY_FILE: 
-                self.ssh.connect(str(self.machine_name), port=self.port, username=self.username, password=self.password, timeout=self.timeout)
-            else: 
-                self.ssh.connect(str(self.machine_name), port=self.port, username=self.username, key_filename=self.KEY_FILE, timeout=self.timeout)
-        except paramiko.AuthenticationException:
-            print str(self.machine_name) + ': authentication failed'
-            return None
-        except Exception as err:
-            print str(self.machine_name) + ': ' + str(err)
-            return  None
-
-   
-    def get_CPU(self):  
+    def get_CPU(self):
         if self.GET_CPU_INFO:
-            stdin, stdout, stderr = self.ssh.exec_command(" sysctl -n hw.model sysctl hw.ncpu")
-            data_out = stdout.readlines()
-            data_err  = stderr.readlines()
+            data_out, data_err = execute(" sysctl -n hw.model sysctl hw.ncpu")
             if not data_err:
                 cpumodel = data_out[0].strip()
                 cpucount = data_out[1].strip()
                 self.sysData.update({'cpumodel':cpumodel})
                 self.sysData.update({'cpucount':cpucount})
-                 
             else:
                 print data_err
-        
-                
 
     def get_RAM(self):
         if self.GET_MEMORY_INFO:
-            stdin, stdout, stderr = self.ssh.exec_command("grep memory /var/run/dmesg.boot")
-            data_out = stdout.readlines()
-            data_err  = stderr.readlines()
+            data_out, data_err = execute("grep memory /var/run/dmesg.boot")
             if not data_err:
                 for rec in data_out:
                     if 'real' in rec:
@@ -87,12 +57,9 @@ class GetBSDData():
                         self.sysData.update({'memory':memory})
             else:
                 print 'Error: ', data_err
-                
-    
+
     def get_name(self):
-        stdin, stdout, stderr = self.ssh.exec_command("/bin/hostname -f")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = execute("/bin/hostname -f")
         if not data_err:
             full_name = data_out[0].strip()
             if self.IGNORE_DOMAIN:
@@ -104,13 +71,10 @@ class GetBSDData():
                 return full_name
         else:
             print 'Error: ', data_err
-            
-    
+
     def get_IP(self):
         addresses = {}
-        stdin, stdout, stderr = self.ssh.exec_command("ifconfig")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = execute("ifconfig")
         if not data_err:
             nics  = []
             tmpv4 = {}
@@ -127,7 +91,7 @@ class GetBSDData():
                         nics.append(tmpv4)
                         tmpv4 = {}
                         tmpv4.update({'device':self.device_name})
-                        tmpv4.update({'tag':device})                        
+                        tmpv4.update({'tag':device})
                     if tmpv6 == {}:
                         tmpv6.update({'device':self.device_name})
                         tmpv6.update({'tag':device})
@@ -153,13 +117,13 @@ class GetBSDData():
                     if rec.strip().startswith('inet6'):
                         ipv6 = rec.split()[1]
                         tmpv6.update({'ipaddress':ipv6})
-                    
+
             nics.append(tmpv4)
             nics.append(tmpv6)
             nics.append(macs)
-           
-            
-            
+
+
+
             for nic in nics:
                 if 'tag' in nic:
                     if nic['tag'].startswith('lo'):
@@ -175,14 +139,10 @@ class GetBSDData():
                             self.allData.append(nic)
         else:
             print 'Error: ', data_err
-            
-            
-        
+
     def get_sys(self):
         self.device_name = self.get_name()
-        stdin, stdout, stderr = self.ssh.exec_command("uname -srK")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = execute("uname -srK")
         if not data_err:
             data = ' '.join(data_out).split()
             os  = data[0].strip()
@@ -192,13 +152,10 @@ class GetBSDData():
             kernel_version = data[2].strip()
             self.sysData.update({'osverno':kernel_version})
             self.sysData.update({'name':self.device_name})
-            
         else:
             print 'Error: ', data_err
-            
-        stdin, stdout, stderr = self.ssh.exec_command("sysctl -n kern.vm_guest ; sysctl -n kern.hostuuid")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+
+        data_out, data_err = execute("sysctl -n kern.vm_guest ; sysctl -n kern.hostuuid")
         if not data_err:
             uuid = data_out[1].strip()
             self.sysData.update({'uuid':uuid})
@@ -210,10 +167,10 @@ class GetBSDData():
             elif 'none' in virt:
                 manufacturer = 'physical'
             self.sysData.update({'type':manufacturer})
-   
+
         else:
             print 'Error: ', data_err
-        
+
 
 
 
