00bdc3f6a228541d518392c74625fc8338379a5b
Brook, Andy [BSD] - CRI <abrook@bsd.uchicago.edu>
2016-01-20 11:15:07 -0600

    Update module_solaris.py to use fabric instead of paramiko

--- module_solaris.py
+++ module_solaris.py
@@ -1,19 +1,9 @@
-import paramiko
-
-
+# Import Fabric API
+from fabric.api import *
 
 class GetSolarisData():
-    def __init__(self,  ip, SSH_PORT, TIMEOUT, usr, pwd, USE_KEY_FILE, KEY_FILE, \
-                    GET_SERIAL_INFO, GET_HARDWARE_INFO, GET_OS_DETAILS, \
+    def __init__(self, GET_SERIAL_INFO, GET_HARDWARE_INFO, GET_OS_DETAILS, \
                     GET_CPU_INFO, GET_MEMORY_INFO, IGNORE_DOMAIN, UPLOAD_IPV6, DEBUG):
-        self.machine_name       = ip
-        self.port               = int(SSH_PORT)
-        self.timeout            = TIMEOUT
-        self.username           = usr
-        self.password           = pwd
-        self.ssh                = paramiko.SSHClient()
-        self.USE_KEY_FILE       = USE_KEY_FILE
-        self.KEY_FILE           = KEY_FILE
         self.GET_SERIAL_INFO    = GET_SERIAL_INFO
         self.GET_HARDWARE_INFO  = GET_HARDWARE_INFO
         self.GET_OS_DETAILS     = GET_OS_DETAILS
@@ -22,42 +12,27 @@ class GetSolarisData():
         self.IGNORE_DOMAIN      = IGNORE_DOMAIN
         self.UPLOAD_IPV6        = UPLOAD_IPV6
         self.DEBUG              = DEBUG
-        self.ssh                = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.conn       = None
         self.sysData    = {}
         self.allData    = []
 
-
     def main(self):
-        self.connect()
         self.get_sys()
         self.get_CPU()
         self.get_RAM()
-        self.allData.append(self.sysData)    
+        self.allData.append(self.sysData)
         self.get_IP()
         return self.allData
 
+    def execute(self, cmd):
+        # Since there seems to be no sudo commands for this module
+        output = run(cmd)
+        data_err = output.stderr
+        data_out = output.stdout
+        return data_out.splitlines(),data_err.splitlines()
 
-    def connect(self):
-        try:
-            if not self.USE_KEY_FILE: 
-                self.ssh.connect(str(self.machine_name), port=self.port, username=self.username, password=self.password, timeout=self.timeout)
-            else: 
-                self.ssh.connect(str(self.machine_name), port=self.port, username=self.username, key_filename=self.KEY_FILE, timeout=self.timeout)
-        except paramiko.AuthenticationException:
-            print str(self.machine_name) + ': authentication failed'
-            return None
-        except Exception as err:
-            print str(self.machine_name) + ': ' + str(err)
-            return  None
-
-   
-    def get_CPU(self):  
+    def get_CPU(self):
         if self.GET_CPU_INFO:
-            stdin, stdout, stderr = self.ssh.exec_command("/usr/bin/kstat cpu_info")
-            data_out = stdout.readlines()
-            data_err  = stderr.readlines()
+            data_out, data_err = self.execute("/usr/bin/kstat cpu_info")
             if not data_err:
                 cpu_ids = []
                 core_ids = []
@@ -72,15 +47,13 @@ class GetSolarisData():
                 cpucores  = len(set(core_ids))
                 self.sysData.update({'cpupower':cpupower})
                 self.sysData.update({'cpucount':cpucount})
-                self.sysData.update({'cpucore':cpucores})  
+                self.sysData.update({'cpucore':cpucores})
             else:
                 print data_err
 
     def get_RAM(self):
         if self.GET_MEMORY_INFO:
-            stdin, stdout, stderr = self.ssh.exec_command("/usr/sbin/prtconf")
-            data_out = stdout.readlines()
-            data_err  = stderr.readlines()
+            data_out, data_err = self.execute("/usr/sbin/prtconf")
             if not data_err:
                 for rec in data_out:
                     if 'Memory ' in rec:
@@ -88,23 +61,17 @@ class GetSolarisData():
                         self.sysData.update({'memory':memory})
             else:
                 print 'Error: ', data_err
-    
-    
+
     def get_name(self):
-        stdin, stdout, stderr = self.ssh.exec_command("/usr/bin/hostname")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = execute("/usr/bin/hostname")
         if not data_err:
             return data_out[0].strip()
         else:
             print 'Error: ', data_err
-            
-            
+
     def get_macs(self):
         macs = {}
-        stdin, stdout, stderr = self.ssh.exec_command("/usr/sbin/dladm show-phys -m")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = self.execute("/usr/sbin/dladm show-phys -m")
         if not data_err:
             for rec in data_out[1:]:
                 nic, slot, address, in_use, client = rec.split()
@@ -115,9 +82,7 @@ class GetSolarisData():
                 macs.update({nic:address})
             return macs
         else:
-            stdin, stdout, stderr = self.ssh.exec_command("/usr/sbin/arp -a")
-            data_out = stdout.readlines()
-            data_err  = stderr.readlines()
+            data_out, data_err = self.execute("/usr/sbin/arp -a")
             if not data_err:
                 for rec in data_out[1:]:
                         nic = rec.split()[0]
@@ -127,12 +92,9 @@ class GetSolarisData():
             else:
                 print 'Error: ', data_err
 
-
     def get_IP(self):
         macs = self.get_macs()
-        stdin, stdout, stderr = self.ssh.exec_command("/usr/sbin/ifconfig -a")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = self.execute("/usr/sbin/ifconfig -a")
         if not data_err:
             name = self.get_name()
             n = 0
@@ -152,16 +114,13 @@ class GetSolarisData():
                             macData = {}
                             if not mac:
                                 mac = ''
-
                             if '/' in ip: # ipv6
                                 ip = ip.split('/')[0]
-
                             nicData.update({'ipaddress':ip})
                             nicData.update({'macaddress':mac})
                             nicData.update({'device':name})
                             nicData.update({'tag':nic})
                             self.allData.append(nicData)
-
                             if mac != '':
                                 macData.update({'macaddress':mac})
                                 macData.update({'port_name':nic})
@@ -171,11 +130,8 @@ class GetSolarisData():
         else:
             print 'Error: ', data_err
 
-        
     def get_sys(self):
-        stdin, stdout, stderr = self.ssh.exec_command("uname -X")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = self.execute("uname -X")
         if not data_err:
             for rec in data_out:
                 if 'System ' in rec:
@@ -195,10 +151,7 @@ class GetSolarisData():
                     self.sysData.update({'name':name})
         else:
             print 'Error: ', data_err
-            
-        stdin, stdout, stderr = self.ssh.exec_command("/usr/sbin/smbios -t SMB_TYPE_SYSTEM")
-        data_out = stdout.readlines()
-        data_err  = stderr.readlines()
+        data_out, data_err = self.execute("/usr/sbin/smbios -t SMB_TYPE_SYSTEM")
         if not data_err:
             for rec in data_out:
                 if 'Manufacturer:' in rec:
@@ -207,7 +160,7 @@ class GetSolarisData():
                         if mftr.lower() == manufacturer.lower():
                             manufacturer = 'virtual'
                             self.sysData.update({'manufacturer':'virtual'})
-                            break    
+                            break
                         if manufacturer != 'virtual':
                             self.sysData.update({'manufacturer':manufacturer})
                 if 'Product:' in rec:
@@ -223,16 +176,3 @@ class GetSolarisData():
                     self.sysData.update({'uuid':uuid})
         else:
             print 'Error: ', data_err
-        
-
-
-
-
-
-
-
-
-
-
-
-
